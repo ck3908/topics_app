89932bc33b7b7b593026477bc58e40c8e767a663
Andrew G <ck3908@gmail.com>
2013-09-28 16:05:24 -0400

    properly added functionality to delete reviews, used delete method and proper delete path

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -49,11 +49,10 @@ class ReviewsController < ApplicationController
   # DELETE /reviews/1
   # DELETE /reviews/1.json
   def destroy
+    @topic = Topic.find(params[:topic_id])
+    @review = @topic.reviews.find(params[:id])
     @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url }
-      format.json { head :no_content }
-    end
+    redirect_to idea_path(@topic)
   end
 
   private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
 class Review < ActiveRecord::Base
   belongs_to :topic
+  before_save :rating_check
+
+  # make sure ratings are in a range before save
+  def rating_check
+    if self.rating > 10
+      self.rating = 10
+    end
+    if self.rating < 1
+      self.rating = 1
+    end
+  end
+
 end
