83064764046ca56e71a240ad571f0e2b04163516
Andrew G <ck3908@gmail.com>
2013-10-03 03:41:59 -0400

    image test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,7 @@ Concept::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  # try this changing precompile
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
